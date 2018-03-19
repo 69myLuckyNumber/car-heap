b907a6e87efba4d37867ab20671d3c9fba7ab4e3
Yurnero <muravyoov@gmail.com>
2018-03-19 15:31:23 +0200

    Add Vehicle repository and corresponding api

--- Core/Abstract/IVehicleRepository.cs
+++ Core/Abstract/IVehicleRepository.cs
@@ -6,5 +6,7 @@ namespace car_heap.Core.Abstract
     public interface IVehicleRepository
     {
         Task AddVehicleAsync(Vehicle vehicle);
+
+        Task<Vehicle> GetVehicleAsync(int id, bool includeRelated = true);
     }
 }
--- Persistence/Repositories/VehicleRepository.cs
+++ Persistence/Repositories/VehicleRepository.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using car_heap.Core.Abstract;
 using car_heap.Core.Models;
@@ -16,6 +17,22 @@ namespace car_heap.Persistence.Repositories
         public async Task AddVehicleAsync(Vehicle vehicle)
         {
             await context.Vehicles.AddAsync(vehicle);
+        }
+
+        public async Task<Vehicle> GetVehicleAsync(int id, bool includeRelated = true)
+        {
+            if(!includeRelated)
+                return await context.Vehicles.SingleOrDefaultAsync(v => v.VehicleId == id);
+            return await context.Vehicles
+                .Include(v => v.Model)
+                    .ThenInclude(m => m.Make)
+                .Include(v => v.Features)
+                    .ThenInclude(vf => vf.Feature)
+                .Include(v => v.Orders)
+                    .ThenInclude(o => o.Status)
+                .Include(v => v.User)
+                    .ThenInclude(v => v.Contact)
+                .SingleOrDefaultAsync(v => v.VehicleId == id);
         }
     }
 }
