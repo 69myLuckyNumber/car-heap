745b04e25374b4d58ed78cd60ceb48d0d71f7392
Yurnero <muravyoov@gmail.com>
2018-03-21 21:58:38 +0200

    Add claim constants

---
+++ Infrastructure/ConfigPocos/AppConstants.cs
@@ -0,0 +1,18 @@
+namespace car_heap.Infrastructure.ConfigPocos
+{
+    public static class AppConstants
+    {
+        public static class Strings
+        {
+            public static class JwtClaimIdentifiers
+            {
+                public const string Rol = "rol", Id = "id";
+            }
+
+            public static class JwtClaims
+            {
+                public const string ApiAccess = "api_access";
+            }
+        }
+    }
+}
