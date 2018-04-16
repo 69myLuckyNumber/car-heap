22b1f05d59f75d53aa5855a2d819c474a4d57dd2
Yurnero <muravyoov@gmail.com>
2018-04-16 17:32:46 +0300

    Fix vehicles list rendering

--- ClientApp/app/components/vehicle-list/vehicle-list.component.ts
+++ ClientApp/app/components/vehicle-list/vehicle-list.component.ts
@@ -4,37 +4,36 @@ import { VehicleService } from '../../services/vehicle.service';
 import { IVehicle } from '../../models/vehicle.model';
 import { AppError } from '../../common/errors/app-error';
 import { NotFoundError } from '../../common/errors/not-found-error';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
 	selector: 'app-vehicle-list',
 	templateUrl: './vehicle-list.component.html',
 	styleUrls: ['./vehicle-list.component.css']
 })
-export class VehicleListComponent implements OnInit {
+export class VehicleListComponent {
 	username: string;
 	vehicles: IVehicle[];
 
 	constructor(private route: ActivatedRoute, 
 		private vehicleService: VehicleService, 
 		private router: Router) {
-
-		route.params.subscribe(p => {
-			this.username = p['username'];
-			if(!this.username) {
-				router.navigate(['/']);
-				return;
-			}
-		})
-	}
-
-	ngOnInit() {
-		this.vehicleService.getUserVehicles(this.username)
-			.subscribe(vehicles => this.vehicles = vehicles,
+		
+		Observable.combineLatest(this.route.params)
+			.switchMap(params => {
+				this.username = params[0]['username'];
+				if(!this.username) {
+					router.navigate(['/']);
+					return;
+				}
+				return this.vehicleService.getUserVehicles(this.username);
+			}).subscribe(vehicles => this.vehicles = vehicles,
 				(error: AppError) => {
-					console.log(error);
 					if(error instanceof NotFoundError)
 						this.router.navigate(['not-found']);
-				})
+				});
 	}
-
 }
